56204f08196575fcb9268f68a8f2cf09fa4f6a32
Min Hee  Park <minheekangg@gmail.com>
2018-10-18 20:34:39 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -7,7 +7,7 @@ class Pokemon
     self.send(("#{key}="),value)}
   end
 
-  def self.save(name, type, database_connection)
+  def save(name, type, database_connection)
     database_connection.execute("INSERT INTO cats (name, type) VALUES (?, ?)",name, type)
   end
 
